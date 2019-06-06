c8c93a8e5634fe5e57fd38a0976d1a47b2a4fe1f
RogueNe0n <50839056+RogueNe0n@users.noreply.github.com>
2019-06-06 14:23:59 -0400

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'NTU5ODc5OTEyMzU4Njc0NDMy.XPK0Cg.HI29Cyr8omutmg0ZGT3-TvZ9LnE' });
-
-manager.spawn(4);
-manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
