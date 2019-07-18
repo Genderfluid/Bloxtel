0136a9ff8389c893ea08943c0c46abb29b98f559
1nvertedNeon <50839056+1nvertedNeon@users.noreply.github.com>
2019-07-18 17:33:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,6 +90,26 @@ client.on("message", msg => {
     var mc = msg.content.split(" ")[1];
     msg.channel.bulkDelete(mc);
   }
+    
+    
+   if (msg.content.startsWith(prefix + "userinfo")) {
+    let member = msg.mentions.users.first() || msg.author;
+    let userembed = new Discord.RichEmbed()
+        .setColor(msg.guild.member(member).highestRole.color)
+        .setThumbnail(member.displayAvatarURL)
+        
+        .setTitle(`Here is ${member.username}'s info.`)
+        .addField(`Name:`, member.username, true)
+        .addField(`Id:`, member.id, true)
+        .addField(`Bot:`, member.bot ? "Yes" : "No", true)
+        .addField("Game:", msg.guild.member(member).presence.game ? msg.guild.member(member).presence.game.name : "Not Playing", true)
+        .addField("Nickname:", msg.guild.member(member).nickname ? msg.guild.member(member).nickname : "None", true )
+        .addField("Last Messsage:", member.lastMessage, true)
+        .addField(`Roles:`, msg.guild.member(member).roles.map(s => s).join(" | "), true)
+        .addField("Created At:", member.createdAt, true)
+        .addField("JoinedAt:", msg.guild.member(member).joinedAt, true)
+        msg.channel.send(userembed);
+   }
 
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'Join-Leave');
@@ -98,7 +118,6 @@ client.on('guildMemberAdd', member => {
 });
 
     
-client.on('message', async (msg) => {
 if (msg.content.startsWith(prefix + "userinfo")) {
     let member = msg.mentions.users.first() || msg.author;
     let userembed = new Discord.RichEmbed()
