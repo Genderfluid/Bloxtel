ea3ee10a3b44e7e11f664f8070dede2d40ede55b
1nvertedNeon <50839056+1nvertedNeon@users.noreply.github.com>
2019-07-20 15:56:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const Discord = require("discord.js"),
 
 client.on("ready",async() =>{
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-    client.user.setActivity (`Bloxtel Support Guild | ;help`, {type:3}) 
+    client.user.setActivity ('${client.guilds.size} guilds' | ;help`, {type:3}) 
 });
 
 client.on("message",async(message) => {
