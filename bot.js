a7cba426cf5a25a62d52352307af413dd93f1bd9
1nvertedNeon <50839056+1nvertedNeon@users.noreply.github.com>
2019-07-20 14:42:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,18 @@ client.on("message",async(message) => {
 client.on("ready", () => {
   console.log("ready to rumble!");
 });
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
 
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
 
 var bannedwords = "buster".split(",");
 
