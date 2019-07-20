c88ee6f7e943c8de286be1ff6b9b083b6511352f
1nvertedNeon <50839056+1nvertedNeon@users.noreply.github.com>
2019-07-20 15:27:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'NTU5ODc5OTEyMzU4Njc0NDMy.XPK0Cg.HI29Cyr8omutmg0ZGT3-TvZ9LnE' });
+const manager = new ShardingManager('./bot.js', { token : 'NTU5ODc5OTEyMzU4Njc0NDMy.XTNpkA.8tv_AM4adylmJBKH7ZtzEEY9mh8' });
 
 manager.spawn(4);
 manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
