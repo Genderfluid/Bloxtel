8e036ba0f6f669205fc016a028f9f094bca1edbf
1nvertedNeon <50839056+1nvertedNeon@users.noreply.github.com>
2019-07-20 15:29:47 -0400

    Delete ping.js

--- Commands/ping.js
+++
@@ -1,8 +0,0 @@
-client.on("message", (message)){ // EventEmitter
-	if(message.content == "!ping"){ // Check if message is "!ping"
-			message.channel.send("Pinging ...") // Placeholder for pinging ... 
-			.then((msg) => { // Resolve promise
-				msg.edit("Ping: " + (Date.now() - msg.createdTimestamp)) // Edits message with current timestamp minus timestamp of message
-			});
-		}
-}
