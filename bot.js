754738485ad0d9270117504c3934284cb7f32df8
1nvertedNeon <50839056+1nvertedNeon@users.noreply.github.com>
2019-07-22 15:21:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js"),
       prefix = ";",
     token = "";
 
-const config = require('./config.js');
+const config = require('./config.json');
 
 
 client.on("ready",async() =>{
