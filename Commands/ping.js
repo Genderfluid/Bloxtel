dd389c121d96ef6101a823deccc7722fa4ec48ae
1nvertedNeon <50839056+1nvertedNeon@users.noreply.github.com>
2019-09-14 16:06:00 -0400

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,3 +1,16 @@
-module.exports = (message) => { // Function with 'message' parameter
-	message.channel.send("Pong!").catch(e => console.log(e));
+exports.name = "ping"
+exports.level = 0; 
+
+
+exports.command = function(client, message) {
+    var start = new Date().getTime()
+    message.channel.send("Pinging...").then((message) => {
+        var end = new Date().getTime()
+        message.edit(":ping_pong: Pong! Took **" + (end - start) + "**ms");
+    })
+  
+  // Permission check
+       if(!message.guild.members.get(client.user.id).hasPermission('EMBED_LINKS')){
+          message.channel.send(':warning: I don\'t have the `Embed Links` permission! I need this to run most commands.')
+ }
 }
